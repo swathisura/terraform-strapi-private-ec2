23248f5c36451a1d036bf8533b60ac7e19d53bee
swathisura <swathisura65@gmail.com>
2026-02-06 12:06:27 +0530

    Create variables.tf

---
+++ modules/alb/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_id" {
+  type = string
+}
+
+variable "alb_sg_id" {
+  type = string
+}
+
+variable "target_instance_id" {
+  type = string
+}
