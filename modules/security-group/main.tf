e86bb2c0cd0a3e3d4edc9c86c1cfeebad4ea810c
swathisura <swathisura65@gmail.com>
2026-02-06 11:55:21 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -21,4 +21,27 @@ resource "aws_security_group" "alb_sg" {
     Name = "alb-sg"
   }
 }
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "EC2 security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 1337
+    to_port         = 1337
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
 
