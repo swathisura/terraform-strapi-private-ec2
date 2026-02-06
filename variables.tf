249b46f9dd73a40b9cfc54e03a70c15c55998a30
swathisura <swathisura65@gmail.com>
2026-02-06 11:45:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability Zone"
+  type        = string
+}
 
