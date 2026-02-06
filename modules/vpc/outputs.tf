6bbf588ea73b6ed521cedb96e017e4c13fd9a4e4
swathisura <swathisura65@gmail.com>
2026-02-06 11:43:32 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,1 +1,15 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  description = "Public Subnet ID"
+  value       = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  description = "Private Subnet ID"
+  value       = aws_subnet.private.id
+}
 
