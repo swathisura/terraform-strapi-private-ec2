01013791b821acd879cb9d7a187447f59ad134ba
swathisura <swathisura65@gmail.com>
2026-02-06 12:03:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,4 +11,13 @@ module "security_group" {
 
   vpc_id = module.vpc.vpc_id
 }
+module "ec2" {
+  source = "./modules/ec2"
+
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  subnet_id         = module.vpc.private_subnet_id
+  security_group_id = module.security_group.ec2_sg_id
+  key_name          = var.key_name
+}
 
