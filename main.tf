4d27707d72af7f1e55d2fd8b5f362204ded50431
swathisura <swathisura65@gmail.com>
2026-02-06 11:44:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  az                  = var.az
+}
 
