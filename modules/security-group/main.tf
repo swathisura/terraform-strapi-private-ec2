016d0566dbb0090c70053ed17018259d5abb4b99
swathisura <swathisura65@gmail.com>
2026-02-06 11:54:27 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,1 +1,24 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "ALB security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
 
